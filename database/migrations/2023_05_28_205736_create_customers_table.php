7855d93057b223c8eab70af7f496622db14d9763
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 23:01:10 +0200

    laravel

--- database/migrations/2023_05_28_205736_create_customers_table.php
+++ database/migrations/2023_05_28_205736_create_customers_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
