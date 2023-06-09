a70bde90936623c854ea18125eb491b493655fc1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-09 12:55:02 +0200

    laravel

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('customers',CustomerController::class);
